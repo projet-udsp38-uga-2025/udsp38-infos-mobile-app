638e4465461157b454203a9789a74f7212d4a3cb
almahdi.mouhammad@etu.grenoble-alpes.fr <almahdi.mouhammad@etu.univ-grenoble-alpes.fr>
2025-11-10 22:00:13 +0100

    chore: ajout des dependances

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import '../models/notif.dart';
 =======
 >>>>>>> 754a7dd (mise en place de larchi mvc)
+=======
+import '../models/notif.dart';
+>>>>>>> 75cab82 (ajout des dependances)
 
 class FirebaseService {
     final _messaging = FirebaseMessaging.instance; 
