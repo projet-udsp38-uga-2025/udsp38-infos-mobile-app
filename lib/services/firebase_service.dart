0822ba31553788793f2e42bac02f804cea69f583
almahdi.mouhammad@etu.grenoble-alpes.fr <almahdi.mouhammad@etu.univ-grenoble-alpes.fr>
2025-11-10 21:52:49 +0100

    chore: mise en place de l'architecture MVC

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+<<<<<<< HEAD
+import '../models/notif.dart';
+=======
+>>>>>>> 754a7dd (mise en place de larchi mvc)
+
+class FirebaseService {
+    final _messaging = FirebaseMessaging.instance; 
+
+    Future<void> init() async {
+        await _messaging.requestPermission();
+
+        FirebaseMessaging.onMessage.listen((message) {
+            print("Message reçu : ${message.notification?.title}");
+        }); 
+    } 
+
+    Notif? convertToNotif(RemoteMessage message) {
+        return Notif(
+            title: message.notification?.title ?? "",
+            body: message.notification?.body ?? "",
+        );
+    }
+
+}
