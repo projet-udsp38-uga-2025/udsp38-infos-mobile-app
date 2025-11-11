19ba0dc8358300b5faaf4f48f03bd7cec83c4858
Abdraman Abakar <150161319+l3miage-abdramaa@users.noreply.github.com>
2025-11-11 10:57:08 +0100

    chore: mise en place de l'architecture MVC, ajout des dépendances et résolution des conflits
    * chore: mise en place de l'architecture MVC
    
    * chore: ajout des dependances
    
    * chore: restaurer pubspec avec toutes les dépendances
    
    * fix: resolve conflict in firebase_service.dart
    
    ---------
    
    Co-authored-by: almahdi.mouhammad@etu.grenoble-alpes.fr <almahdi.mouhammad@etu.univ-grenoble-alpes.fr>

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import '../models/notif.dart';
+
+
+class FirebaseService {
+    final _messaging = FirebaseMessaging.instance; 
+
+    Future<void> init() async {
+        await _messaging.requestPermission();
+
+        FirebaseMessaging.onMessage.listen((message) {
+            print("Message reçu : ${message.notification?.title}");
+        }); 
+    } 
+
+    Notif? convertToNotif(RemoteMessage message) {
+        return Notif(
+            title: message.notification?.title ?? "",
+            body: message.notification?.body ?? "",
+        );
+    }
+
+}
